37f2bd8df1858df1c341254dee4538fbbaa752bd
VoloshenyukIgor <voloshenyukigor92@gmail.com>
2018-01-25 20:18:16 +0200

    1

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  users = [
+    {name: 'WFM 1' },
+    {name: 'WFM 2' },
+    {name: 'WFM 3' },
+    {name: 'WFM 4' },
+    {name: 'WFM 5' }
+  ]
+}
